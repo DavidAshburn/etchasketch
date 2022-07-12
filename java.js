f6a11eb06da662f86da83a68f13ead019f101a8c
DavidAshburn <oneinchwebbing@gmail.com>
2022-07-12 12:02:24 -0700

    grid works, mouseenter highlight doesnt

--- java.js
+++ java.js
@@ -1,6 +1,7 @@
 const gridHead = document.querySelector('.header');
 const gridLabel = document.querySelector('.label');
 const gridContainer = document.querySelector('.grid-container')
+const resetBtn = document.querySelector('.reset');
 
 let wallSize = 16;
 
@@ -8,10 +9,10 @@ const sketchCell = () => this.classList.add("activeCell");
 
 function resizeGrid () {
 
-	let newSize = prompt("Please enter a new grid size.", "2 - 100 only" );
+	let newSize = prompt("Please enter a new grid size. 2 - 100");
 
 	const currentField = document.querySelectorAll('.grid-column');
-	for(let i=0;, i< currentField.length; i++)
+	for(let i=0; i< currentField.length; i++)
 		{currentField[i].remove();}
 
 	for(let c = 0; c < newSize; c++)
@@ -30,5 +31,6 @@ function resizeGrid () {
 	}
 }
 
-const resetBtn = document.querySelector('.reset');
+
 resetBtn.addEventListener('click', resizeGrid);
+resetBtn.addEventListener('click', () => console.log("click"));
